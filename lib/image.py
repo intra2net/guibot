6dd3f71bdf7b5787372a4aa49395ae3b29a99957
Thomas Jarosch <thomas.jarosch@intra2net.com>
2013-01-19 21:32:14 +0100

    Implement Image class and unit test. Created example images

---
+++ lib/image.py
@@ -0,0 +1,77 @@
+# Copyright 2013 Intranet AG / Thomas Jarosch
+#
+# guibender is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# guibender is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with guibender.  If not, see <http://www.gnu.org/licenses/>.
+#
+import copy
+import autopy.bitmap
+from location import Location
+
+class Image:
+    DEFAULT_SIMILARITY = 0.95
+
+    def __init__(self, filename_or_Image=None, similarity=DEFAULT_SIMILARITY, backend_data=None):
+        self.filename = filename_or_Image
+        self.img_similarity = similarity
+        self.backend_data = backend_data
+
+        self.width = 0
+        self.height = 0
+        self.target_center_offset = Location(0, 0)
+
+        if self.filename is not None and backend_data is None:
+            # TODO: Load just once and store in a global ImageCache instance
+            # TODO: Abstract out autopy backend into separate backend class
+            # TODO: Implement image "finder"
+            self.backend_data = autopy.bitmap.Bitmap.open(self.filename)
+
+        # Set width and height
+        if self.backend_data:
+            self.width = self.backend_data.width
+            self.height = self.backend_data.height
+
+    def copy(self):
+        return copy.copy(self)
+
+    def get_filename(self):
+        return self.filename;
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
+
+    def get_backend_data(self):
+        return self.backend_data
+
+    def get_similarity(self):
+        return self.img_similarity
+
+    def similarity(self, new_similarity):
+        new_image = self.copy()
+
+        new_image.img_similarity = new_similarity
+        return new_image
+
+    def exact(self):
+        return self.similarity(1.0)
+
+    def target_offset(self, xpos, ypos):
+        new_image = self.copy()
+
+        new_image.target_center_offset = Location(xpos, ypos)
+        return new_image
+
+    def get_target_offset(self):
+        return self.target_center_offset
---
+++ tests/test_image.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+# Copyright 2013 Intranet AG / Thomas Jarosch
+#
+# guibender is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# guibender is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with guibender.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import unittest
+import sys
+sys.path.append('../lib')
+
+from image import Image
+
+class ImageTest(unittest.TestCase):
+    def setUp(self):
+        self.example_dir = '../examples/images/'
+
+    def test_basic(self):
+        image = Image(self.example_dir + 'all_shapes.png')
+
+        self.assertEqual(400, image.get_width())
+        self.assertEqual(300, image.get_height())
+
+        self.assertTrue(image.get_filename().find('all_shapes.png') is not -1)
+        self.assertEqual(Image.DEFAULT_SIMILARITY, image.get_similarity())
+
+    def test_copy_object(self):
+        image = Image(self.example_dir + 'all_shapes.png')
+
+        my_copy = image.copy()
+        self.assertEqual(image.filename, my_copy.filename)
+        self.assertEqual(image.img_similarity, my_copy.img_similarity)
+        self.assertEqual(image.backend_data, my_copy.backend_data)
+        self.assertEqual(image.width, my_copy.width)
+        self.assertEqual(image.height, my_copy.height)
+        self.assertEqual(image.target_center_offset, my_copy.target_center_offset)
+
+    def test_target_offset(self):
+        image = Image(self.example_dir + 'all_shapes.png')
+
+        target_offset = image.get_target_offset()
+        self.assertEqual(0, target_offset.get_x())
+        self.assertEqual(0, target_offset.get_y())
+
+        new_image = image.target_offset(100, 30)
+        self.assertEqual(image.filename, new_image.filename)
+        self.assertEqual(image.img_similarity, new_image.img_similarity)
+        self.assertEqual(image.backend_data, new_image.backend_data)
+        self.assertEqual(image.width, new_image.width)
+        self.assertEqual(image.height, new_image.height)
+        self.assertNotEqual(image.target_center_offset, new_image.target_center_offset)
+
+        target_offset = new_image.get_target_offset()
+        self.assertEqual(100, target_offset.get_x())
+        self.assertEqual(30, target_offset.get_y())
+
+        # check it's unchanged in the original
+        target_offset = image.get_target_offset()
+        self.assertEqual(0, target_offset.get_x())
+        self.assertEqual(0, target_offset.get_y())
+
+    def test_similarity(self):
+        image = Image(self.example_dir + 'all_shapes.png')
+
+        new_image = image.similarity(0.45)
+        self.assertEqual(0.45, new_image.get_similarity())
+        self.assertEqual(Image.DEFAULT_SIMILARITY, image.get_similarity())
+
+        self.assertEqual(image.filename, new_image.filename)
+        self.assertNotEqual(image.img_similarity, new_image.img_similarity)
+        self.assertEqual(image.backend_data, new_image.backend_data)
+        self.assertEqual(image.width, new_image.width)
+        self.assertEqual(image.height, new_image.height)
+        self.assertEqual(image.target_center_offset, new_image.target_center_offset)
+
+    def test_exact(self):
+        image = Image(self.example_dir + 'all_shapes.png')
+
+        new_image = image.exact()
+        self.assertEqual(1.0, new_image.get_similarity())
+        self.assertEqual(Image.DEFAULT_SIMILARITY, image.get_similarity())
+
+if __name__ == '__main__':
+    unittest.main()
