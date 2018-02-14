28c25478e15d5c0f4c3bbb9caf4e00f4d9379ba9
Plamen Dimitrov <pdimitrov@pevogam.com>
2018-02-14 23:50:00 +0200

    Use the README as index file for the documentation on RTD

---
+++ doc/api/README.rst
@@ -0,0 +1,11 @@
+# guibot
+
+**guibot** is a tool for GUI automation from testing to performing mundane tasks using OpenCV and PyTorch.
+
+Homepage: http://guibot.org
+
+Documentation: http://guibot.readthedocs.io
+
+Installation: https://github.com/intra2net/guibot/blob/master/packaging/backend_dependencies.lst
+
+Issue tracking: https://github.com/intra2net/guibot/issues
--- doc/api/generate_api.sh
+++ doc/api/generate_api.sh
@@ -3,9 +3,13 @@
 # produce rst files for the modules
 sphinx-apidoc -e -f -o . ../../guibot || die "No rst files could be generated"
 
+# move all rst files to source directory to integrate with RTD
+rm -fr source README.rst
+mkdir source || die "No source directory to move rst files to"
+mv *.rst source
+
 # produce HTML documentation from the rst files
 make html
 
-# move all rst files to source directory to integrate with RTD
-mkdir source || die "No source directory to move rst files to"
-mv *.rst source
+# use README as index page for RTD (needs MD-RST compatibility)
+pandoc ../../README.md --from markdown --to rst -s -o README.rst
