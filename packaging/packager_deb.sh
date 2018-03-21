ffe7436e7146acad8ef025de08908696555d7789
Plamen Dimitrov <pdimitrov@pevogam.com>
2018-03-21 22:18:45 +0200

    Add rpm/deb package dependencies to the platform specific packagers

--- packaging/packager_deb.sh
+++ packaging/packager_deb.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+set -e
+
+# dep dependencies
+apt-get update
+# python2.7
+# in some cases another repo has to be added
+#apt-get -y install software-properties-common
+#add-apt-repository ppa:fkrull/deadsnakes-python2.7
+apt-get -y install python2.7
+# python-imaging
+apt-get -y install python-pil
+# contour, template, feature, cascade, text matching
+apt-get -y install python-numpy python-opencv
+# text matching
+apt-get -y install tesseract-ocr
 
 exit 0
--- packaging/packager_rpm.sh
+++ packaging/packager_rpm.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+set -e
+
+# rpm dependencies
+# python2.7
+dnf -y install python
+# python-imaging
+dnf -y install python-pillow
+# contour, template, feature, cascade, text matching
+dnf -y install python2-numpy opencv-python
+# text matching
+dnf -y install tesseract
 
 exit 0
