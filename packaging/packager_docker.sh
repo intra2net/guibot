a583aa4a8cce7eaedf3ef9219b104ede98179ed1
Plamen Dimitrov <pdimitrov@pevogam.com>
2018-05-17 22:10:34 +0700

    Fix docker image versions of Fedora and Ubuntu to a particular version
    Using the latest versions can break the code due to API changes and
    other external reasons irrelevant to code changes within the tested
    branch. Let's fix the version and move to a newer one when we can
    confirm we support it.

--- packaging/packager_docker.sh
+++ packaging/packager_docker.sh
@@ -4,10 +4,10 @@ readonly install_variant="${INSTALL_VARIANT:-pip}"
 
 if [ "$install_variant" == "rpm" ]; then
     sudo docker run \
-            -v $(pwd)/..:/guibot:rw fedora:latest \
+            -v $(pwd)/..:/guibot:rw fedora:27 \
             /bin/bash /guibot/packaging/packager_rpm.sh
 elif [ "$install_variant" == "deb" ]; then
     sudo docker run \
-            -v $(pwd)/..:/guibot:rw ubuntu:latest \
+            -v $(pwd)/..:/guibot:rw ubuntu:xenial \
             /bin/bash /guibot/packaging/packager_deb.sh
 fi
