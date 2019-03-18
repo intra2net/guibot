ea7c8d119889d52115c065dab37090b618b87481
Plamen Dimitrov <pdimitrov@pevogam.com>
2019-03-18 18:19:13 +0800

    Bump some patch (major.minor.patch) versions for python 3.7 availability

--- packaging/packager_rpm.sh
+++ packaging/packager_rpm.sh
@@ -18,7 +18,7 @@ dnf -y install vnc-server
 # pip dependencies (not available as RPM)
 dnf -y install gcc libX11-devel libXtst-devel python3-devel libpng-devel redhat-rpm-config
 pip3 install autopy==1.1.1
-pip3 install torch==0.4.0 torchvision==0.2.1
+pip3 install torch==0.4.1 torchvision==0.2.1
 pip3 install vncdotool==0.12.0
 
 # rpm packaging
