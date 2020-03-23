080516e73dfb002e415a874bde916a54ff563c01
Plamen Dimitrov <pdimitrov@pevogam.com>
2020-03-23 23:13:16 +0200

    Be verbose during unit testing for easier checks on covered backends

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,5 +5,5 @@ set -e
 readonly libpath="${LIBPATH:-../guibot}"
 readonly coverage="${COVERAGE:-coverage3}"
 
-$coverage run --source="$libpath" -m unittest discover -s ../tests/
+$coverage run --source="$libpath" -m unittest discover -v -s ../tests/
 $coverage report -m
