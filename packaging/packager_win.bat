013b0da46db6cddc5dffcfff5fa93869fbe027bd
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2022-01-21 17:33:47 +0200

    Install from an unattended recent local python executable
    We assume the user makes sure to download and install or already
    have installed the Python executable on their system.

--- packaging/packager_win.bat
+++ packaging/packager_win.bat
@@ -5,9 +5,11 @@ SET DISTRO_VERSION="10"
 SET DISTRO_ROOT="%HOMEDRIVE%"
 
 REM Main deps
-python-3.6.6-amd64.exe
+cd %DISTRO_ROOT%\
+REM Assuming a local python executable to avoid overloading the download web page
+START /WAIT python-3.9.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 REM set a temporary path variable valid inside this session
-set PYTHONPATH=C:\Users\Administrator\AppData\Local\Programs\Python\Python36
+set PYTHONPATH="C:\Program Files\Python39"
 set PATH=%PYTHONPATH%;%PYTHONPATH%\Scripts;%PATH%
 REM a permanent path variable will be set by the executable once this batch exits
 
