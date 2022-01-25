b8c83eb0836045817dc87e9709f9bd1a9205cc46
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2022-01-26 00:24:59 +0200

    Add some minimal PyPI release capabilities
    We choose to pass an explicit tag and not autodetect such because
    we want to make sure we can tag for a while without triggering a
    release to PyPI.

--- packaging/packager_pip.sh
+++ packaging/packager_pip.sh
@@ -8,6 +8,7 @@ readonly distro="${DISTRO:-centos}"
 readonly distro_version="${VERSION:-8}"
 readonly distro_root="${ROOT:-$HOME}"
 readonly python_version="${PYTHON_VERSION:-3.8}"
+readonly release_tag="${RELEASE_TAG:-}"
 
 # disable tests and support of backends based on python versions
 if [[ $python_version == '3.9' ]]; then export DISABLE_AUTOPY=1; fi
@@ -37,13 +38,10 @@ pip3 install --upgrade pip
 pip3 install -r "$distro_root/guibot/packaging/pip_requirements.txt"
 
 # pip packaging and installing of current guibot source
-pip3 install wheel
+pip3 install wheel twine
 cd "$distro_root/guibot/packaging"
 python3 setup.py sdist bdist_wheel
-# done for potential future deployment
-cp dist/guibot*.whl "$distro_root/guibot"
-cp dist/guibot*.tar.gz "$distro_root/guibot"
-pip3 install "$distro_root"/guibot/guibot*.whl
+pip3 install dist/guibot*.whl
 
 # virtual display
 dnf install -y xorg-x11-server-Xvfb
@@ -67,5 +65,18 @@ LIBPATH=".." COVERAGE="$COVERAGE" sh coverage_analysis.sh
 # TODO: need supported git provider (e.g. GH actions web hooks) for codecov submissions
 #mv "$distro_root/guibot/.git" /usr/local/lib/python3*/site-packages/guibot/
 #LIBPATH=".." COVERAGE="$COVERAGE" SUBMIT=1 sh coverage_analysis.sh
+
+if [[ -n "$release_tag" ]]; then
+    echo "Releasing version ${release_tag} to PyPI"
+    cd "$distro_root/guibot/packaging"
+    if [[ -z $USER ]]; then echo "No username provided as an environment variable" && exit 1; fi
+    if [[ -z $PASS ]]; then echo "No password provided as an environment variable" && exit 1; fi
+    git_tag="$(git describe)"
+    if [[ "$release_tag" != "$git_tag" ]]; then
+        echo "Selected release version ${release_tag} does not match tag ${git_tag}"
+        exit 1
+    fi
+    twine upload --repository pypi --user "$USER" --password "$PASS" dist/*
+fi
 
 exit 0
