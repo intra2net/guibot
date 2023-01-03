fefbf2e29c1b1ff6ecaa11932d66b754256ebd87
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2023-01-03 13:09:18 +0200

    Provide a minimal CI workflow using GH actions
    The biggest obstacle here was codecov's GH action which had some
    really obscure logging and whose default XML exporting did not
    work out of the box this time, extending our customized coverage
    analysis further.

--- tests/coverage_analysis.sh
+++ tests/coverage_analysis.sh
@@ -9,4 +9,7 @@ readonly submit="${SUBMIT:-0}"
 $coverage run --source="$libpath" -m unittest discover -v -s ../tests/
 # use -i to ignore errors from pythong cache files and other traced dependencies
 $coverage report -m -i
+# codecov is poorly documented and even in verbose mode and various directory changes
+# won't tell us where it runs the xml command and why it ends up not finding it
+$coverage xml
 if [[ $submit == 1 ]]; then codecov; fi
